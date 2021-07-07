407483fa9741248bc9af800b339d01ef98e51f8f
tsn778 <tsn778@126.com>
2021-07-07 13:42:31 +0800

    frist

---
+++ vueblog/src/main/java/com/tsn/controller/BlogController.java
@@ -0,0 +1,21 @@
+package com.tsn.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author tsn
+ * @since 2021-07-06
+ */
+@Controller
+@RequestMapping("/tsn/blog")
+public class BlogController {
+
+}
+
---
+++ vueblog/src/main/java/com/tsn/entity/Blog.java
@@ -0,0 +1,42 @@
+package com.tsn.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author tsn
+ * @since 2021-07-06
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class Blog implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private Long userId;
+
+    private String title;
+
+    private String description;
+
+    private String content;
+
+    private LocalDateTime created;
+
+    private Integer status;
+
+
+}
